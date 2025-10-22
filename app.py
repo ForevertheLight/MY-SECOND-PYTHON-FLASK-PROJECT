fa0419621bd14dfdbe60967b627dea8a4c1db9af
ForevertheLight <fashjosh2004@gmail.com>
2025-10-22 10:40:21 +0100

    I Created 2 endpoints to Update and remove an existing stock

--- app.py
+++ app.py
@@ -33,3 +33,32 @@ def create_new_stock():
     Stock.append(New_stock)
     return New_stock
 
+
+# Update an existing stock
+@app.route("/update/stocks/<int:Stock_ID>", methods=['PUT'])
+def update_stock(Stock_ID):
+    data=request.get_json()
+    for item in Stock:
+        if item["id"] == Stock_ID:
+            item['name'] = data.get('name', item['name'])
+            item['quantity'] = data.get('quantity', item['quantity'])
+            item['unit_price'] = data.get('unit_price', item['unit_price'])
+            item['description'] = data.get('description', item['description'])
+            
+            # Recalculate total price
+            item['total_price'] = item['quantity'] * item['unit_price']
+            
+            return jsonify({"message": "Stock updated successfully", "updated_stock": item})
+    
+    return jsonify({"error": "Stock not found"}), 404
+
+
+# Delete an existing stock
+@app.route("/delete/stocks/<int:stock_id>", methods=['DELETE'])
+def delete_stock(stock_id):
+    for item in Stock:
+        if item["id"] == stock_id:
+            Stock.remove(item)
+            return jsonify({"message": "Stock deleted successfully"})
+    
+    return jsonify({"error": "Stock not found"}), 404
